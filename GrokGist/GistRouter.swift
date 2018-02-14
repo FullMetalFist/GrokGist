3eca17af5504f94f5b38758dea9f46f9c3db370a
Michael Vilabrera <m.vilabrera@gmail.com>
2018-02-14 14:36:18 -0500

    implement basic methods for reaching Github API in APIManager & Router

--- GrokGist/GistRouter.swift
+++ GrokGist/GistRouter.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import Alamofire
+
+enum GistRouter: URLRequestConvertible {
+    static let baseURLString = "https://api.github.com/"
+    
+    case getPublic()
+    
+    func asURLRequest() throws -> URLRequest {
+        var method: HTTPMethod {
+            switch self {
+            case .getPublic:
+                return .get
+            }
+        }
+        
+        let url: URL = {
+            let relativePath: String
+            switch self {
+            case .getPublic():
+                relativePath = "gists/public"
+            }
+            
+            var url = URL(string: GistRouter.baseURLString)!
+            url.appendPathComponent(relativePath)
+            return url
+        }()
+        
+        let params: ([String: Any]?) = {
+            switch self {
+            case .getPublic:
+                return nil
+            }
+        }()
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+        
+        let encoding = JSONEncoding.default
+        return try encoding.encode(urlRequest, with: params)
+    }
+}
--- GrokGist/GithubAPIManager.swift
+++ GrokGist/GithubAPIManager.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import Alamofire
+
+class GithubAPIManager {
+    static let sharedInstance = GithubAPIManager()
+    
+    func printPublicGists() {
+        Alamofire.request(GistRouter.getPublic()).responseString { response in
+            
+            if let receivedString = response.result.value {
+                print(receivedString)
+            }
+        }
+    }
+}
